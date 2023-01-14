bc2c408a3da1ad7e303e497a5a05ec7fb2adb7b6
k2supra <andrijkorolevic@gmail.com>
2023-01-14 15:32:04 +0200

    )

---
+++ fuel.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	//**************FUEL**************//
+	size_t fuel_full, fuel_l500, fuel_l1000, fuel_l1500, fuel_l2000;
+	fuel_full = 300;
+	fuel_l500 = 1;
+	fuel_l1000 = 4;
+	fuel_l1500 = 7;
+	fuel_l2000 = 9;
+}
--- main.cpp
+++ main.cpp
@@ -1,2 +1,21 @@
 #include <iostream>
+#include <fuel.cpp>
 
+using namespace std;
+
+int main() {
+
+
+	cout << fuel_l500;
+	
+
+
+
+	size_t path_AB;
+	size_t path_BC;
+	cout << "Enter path from A to B (km): ", cin >> path_AB;
+	//cout << path_AB;
+	cout << "Enter path from B to C (km): ", cin >> path_BC;
+	//cout << path_BC;
+
+}
