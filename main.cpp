409b6b50371abeca8de7b9ee0e7b687d3f3aa1ea
k2supra <andrijkorolevic@gmail.com>
2023-01-15 15:03:50 +0200

    yeeeeeeeees. LIGHT WEIGHT

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <Windows.h>
+HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
 
 using namespace std;
 
@@ -14,7 +17,7 @@ int main() {
 	
 	//*****************FUEL*****************//
 	size_t fuel_full, fuel_l500, fuel_l1000, fuel_l1500, fuel_l2000, fuel_left;
-	int fuel_in_tank;
+	int fuel_in_tank, fuel_add;
 	fuel_full = 300;
 	fuel_l500 = 1;
 	fuel_l1000 = 4;
@@ -60,23 +63,33 @@ int main() {
 		if (fuel_in_tank < 0)
 		{
 			fuel_in_tank = (path_AB * fuel_l500) - 300;
+			SetConsoleTextAttribute(hConsole, 12);
 			cout << "You will not reach the B from A. You will running out of fuel." << " There was not enough fuel: " << fuel_in_tank;
 		}
 		else if (fuel_in_tank >= 0 || fuel_in_tank > 0)
 		{
-			cout << fuel_in_tank << " (l) will left in tank after travel from A to B";
+			SetConsoleTextAttribute(hConsole, 14);
+			cout << fuel_in_tank << " (l) will left in tank after travel from A to B.";
+			fuel_add = fuel_in_tank - (path_BC * fuel_l500);
+			if (fuel_add < 0)
+			{
+				SetConsoleTextAttribute(hConsole, 12);
+				cout << "\nYou must add " << (path_BC * fuel_l500) - fuel_in_tank << " (l) to reach the C.";
+			}
 		}
 
 
 		if (fuel_full < (path_BC * fuel_l500))
 		{
 			fuel_in_tank = (path_BC * fuel_l500) - 300;
+			SetConsoleTextAttribute(hConsole, 12);
 			cout << "\nYou will not reach the B from C. You will running out of fuel." << " There was not enough fuel: " << fuel_in_tank << " (l)";
 		}
 		else if (fuel_in_tank >= (path_BC * fuel_l500))
 		{
-			fuel_in_tank = 300 - (path_BC * fuel_l500);
-				cout << "\n" << fuel_in_tank << " (l) will left in tan after travel from B to C";
+			fuel_in_tank = fuel_in_tank - (path_BC * fuel_l500);
+			SetConsoleTextAttribute(hConsole, 10);
+				cout << "\n" << fuel_in_tank << " (l) will left in tan after travel from B to C.";
 		}
 	}break;
 
@@ -86,23 +99,33 @@ int main() {
 		if (fuel_in_tank < 0)
 		{
 			fuel_in_tank = (path_AB * fuel_l1000) - 300;
+			SetConsoleTextAttribute(hConsole, 12);
 			cout << "You will not reach the B from A. You will running out of fuel." << " There was not enough fuel: " << fuel_in_tank;
 		}
 		else if (fuel_in_tank >= 0 || fuel_in_tank > 0)
 		{
-			cout << fuel_in_tank << " (l) will left in tank after travel from A to B";
+			SetConsoleTextAttribute(hConsole, 14);
+			cout << fuel_in_tank << " (l) will left in tank after travel from A to B.";
+			fuel_add = fuel_in_tank - (path_BC * fuel_l1000);
+			if (fuel_add < 0)
+			{
+				SetConsoleTextAttribute(hConsole, 12);
+				cout << "\nYou must add " << (path_BC * fuel_l1000) - fuel_in_tank << " (l) to reach the C.";
+			}
 		}
 
 
 		if (fuel_full < (path_BC * fuel_l1000))
 		{
 			fuel_in_tank = (path_BC * fuel_l1000) - 300;
+			SetConsoleTextAttribute(hConsole, 12);
 			cout << "\nYou will not reach the B from C. You will running out of fuel." << " There was not enough fuel: " << fuel_in_tank << " (l)";
 		}
 		else if (fuel_in_tank >= (path_BC * fuel_l1000))
 		{
-			fuel_in_tank = 300 - (path_BC * fuel_l1000);
-			cout << "\n" << fuel_in_tank << " (l) will left in tan after travel from B to C";
+			fuel_in_tank = fuel_in_tank - (path_BC * fuel_l1000);
+			SetConsoleTextAttribute(hConsole, 10);
+			cout << "\n" << fuel_in_tank << " (l) will left in tan after travel from B to C.";
 		}
 	}break;
 
@@ -112,23 +135,33 @@ int main() {
 		if (fuel_in_tank < 0)
 		{
 			fuel_in_tank = (path_AB * fuel_l1500) - 300;
+			SetConsoleTextAttribute(hConsole, 12);
 			cout << "You will not reach the B from A. You will running out of fuel." << " There was not enough fuel: " << fuel_in_tank;
 		}
 		else if (fuel_in_tank >= 0 || fuel_in_tank > 0)
 		{
-			cout << fuel_in_tank << " (l) will left in tank after travel from A to B";
+			SetConsoleTextAttribute(hConsole, 14);
+			cout << fuel_in_tank << " (l) will left in tank after travel from A to B.";
+			fuel_add = fuel_in_tank - (path_BC * fuel_l1500);
+			if (fuel_add < 0)
+			{
+				SetConsoleTextAttribute(hConsole, 12);
+				cout << "\nYou must add " << (path_BC * fuel_l1500) - fuel_in_tank << " (l) to reach the C.";
+			}
 		}
 
 
 		if (fuel_full < (path_BC * fuel_l1500))
 		{
 			fuel_in_tank = (path_BC * fuel_l1500) - 300;
+			SetConsoleTextAttribute(hConsole, 12);
 			cout << "\nYou will not reach the B from C. You will running out of fuel." << " There was not enough fuel: " << fuel_in_tank << " (l)";
 		}
 		else if (fuel_in_tank >= (path_BC * fuel_l1500))
 		{
-			fuel_in_tank = 300 - (path_BC * fuel_l1500);
-			cout << "\n" << fuel_in_tank << " (l) will left in tan after travel from B to C";
+			fuel_in_tank = fuel_in_tank - (path_BC * fuel_l1500);
+			SetConsoleTextAttribute(hConsole, 10);
+			cout << "\n" << fuel_in_tank << " (l) will left in tan after travel from B to C.";
 		}
 	}break;
 
@@ -138,23 +171,33 @@ int main() {
 		if (fuel_in_tank < 0)
 		{
 			fuel_in_tank = (path_AB * fuel_l2000) - 300;
+			SetConsoleTextAttribute(hConsole, 12);
 			cout << "You will not reach the B from A. You will running out of fuel." << " There was not enough fuel: " << fuel_in_tank;
 		}
 		else if (fuel_in_tank >= 0 || fuel_in_tank > 0)
 		{
-			cout << fuel_in_tank << " (l) will left in tank after travel from A to B";
+			SetConsoleTextAttribute(hConsole, 14);
+			cout << fuel_in_tank << " (l) will left in tank after travel from A to B.";
+			fuel_add = fuel_in_tank - (path_BC * fuel_l2000);
+			if (fuel_add < 0)
+			{
+				SetConsoleTextAttribute(hConsole, 12);
+				cout << "\nYou must add " << (path_BC * fuel_l2000) - fuel_in_tank << " (l) to reach the C.";
+			}
 		}
 
 
 		if (fuel_full < (path_BC * fuel_l2000))
 		{
 			fuel_in_tank = (path_BC * fuel_l2000) - 300;
+			SetConsoleTextAttribute(hConsole, 12);
 			cout << "\nYou will not reach the B from C. You will running out of fuel." << " There was not enough fuel: " << fuel_in_tank << " (l)";
 		}
 		else if (fuel_in_tank >= (path_BC * fuel_l2000))
 		{
-			fuel_in_tank = 300 - (path_BC * fuel_l2000);
-			cout << "\n" << fuel_in_tank << " (l) will left in tan after travel from B to C";
+			fuel_in_tank = fuel_in_tank - (path_BC * fuel_l2000);
+			SetConsoleTextAttribute(hConsole, 10);
+			cout << "\n" << fuel_in_tank << " (l) will left in tan after travel from B to C.";
 		}
 	}break;
 	default:
